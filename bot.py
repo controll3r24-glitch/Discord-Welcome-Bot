e891dd4c0b4ad059beaa7670ae28a3eba85f96d0
Controll3r <controll3r24@gmail.com>
2026-09-04 22:50:35 +0200

    Clean up comments and instructions in bot.py
    Removed installation instructions and comments from the top of bot.py.

--- bot.py
+++ bot.py
@@ -1,18 +1,4 @@
-"""
-bot.py
-------
-Bot Discord che invia un'immagine di benvenuto personalizzata quando
-un nuovo utente entra nel server.
 
-INSTALLAZIONE:
-    pip install discord.py Pillow aiohttp
-
-AVVIO:
-    python bot.py
-
-Tutte le impostazioni (token, canale, testi, colori, immagine di sfondo)
-si modificano qui sotto, nella sezione CONFIGURAZIONE.
-"""
 
 import io
 import discord
